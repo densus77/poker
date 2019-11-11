ac57f44d1ded1896fbe3c9629f7e22139a0ed2cc
densus77 <51282952+densus77@users.noreply.github.com>
2019-11-11 20:55:19 +0700

    Update data.php

--- data.php
+++ data.php
@@ -14,8 +14,8 @@ $banner= "\033[1;31m╔═══════════════════
 $bot_poker="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvUHFZ&ZU5KRGkiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvb2NrbGVzLmNvbVwvRE5aQWN6NU1KdkhDc2tqTEVHU2wifQFV8FV8";
 
 $hapus = "of";
-$a = "10";
-$b = "4323";
+$a = "50";
+$b = "";216146
 
-$ping = "40";
-$pong = "1446440";
+$ping = "10";
+$pong = "43231";
