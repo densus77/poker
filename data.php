9477b39631af7efcb572d480f717430688e3cd37
densus77 <51282952+densus77@users.noreply.github.com>
2019-11-12 13:57:30 +0700

    Update data.php

--- data.php
+++ data.php
@@ -14,19 +14,6 @@ $banner= "\033[1;31m╔═══════════════════
 $bot_poker="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvUHFZ&ZU5KRGkiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvb2NrbGVzLmNvbVwvRE5aQWN6NU1KdkhDc2tqTEVHU2wifQFV8FV8";
 
 
-function get_reff($data,$asu){
-  if ($data == 10){
-    $a = array(43228,43229,43230);
-    $hasil = $a[$asu];
-  }elseif ($data == 20){
-    $a = array(86458,86459,86460);
-    $hasil = $a[$asu];
-    }else{
-    echo "error data";
-    exit();
-  }
-  return $hasil;
-}
 
 $hapus = "of";
 $po=rand(1,2)*10;
