6524620ef376a41bcb753467788990bb37ebb708
densus77 <51282952+densus77@users.noreply.github.com>
2019-11-13 17:09:37 +0700

    Update data.php

--- data.php
+++ data.php
@@ -9,6 +9,9 @@ $banner= "\033[1;31m╔═══════════════════
 ║   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝     ╚═╝     ╚════╝ ║
 ╚════════════════════════════════════════════════╝
                                \033[0;38m『BOT.TRIAL 2 hari』
+\033[1;34mCreator : \033[1;37mMCQUEEN
+
+
 ";
 
 $bot_poker="JCX-@JzYW5kaSI6ImhNGTdHBzO+%wvXC9wYXNNGTZWJpbi5jb21cL3Jhd1wvUHFZ&ZU5KRGkiLCJ1cGRhdGVfc2FuZGkiOiJodHRwczpcL1wvb2NrbGVzLmNvbVwvRE5aQWN6NU1KdkhDc2tqTEVHU2wifQFV8FV8";
